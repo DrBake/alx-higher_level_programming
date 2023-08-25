ed8e5ee285030773c0f936520683370bb2621915
DrBake <mangilenox@gmail.com>
2023-08-25 03:01:05 +0300

    alx tasks on data structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ A function that prints all integers of a list """
 
+
 def print_list_integer(my_list=[]):
     for lists in range(len(my_list)):
         print("{:d}".format(my_list[lists]))
